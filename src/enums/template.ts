f182ccc4a6ec7c704cdd7090ac5f2d922f7105b0
Richard Annand <rj170590@gmail.com>
2019-03-01 22:16:46 +0000

    Added Sensor Metadata Template

--- src/enums/template.ts
+++ src/enums/template.ts
@@ -4,5 +4,6 @@ export enum TemplateFile {
     ReadMe = 'README.md.tmpl',
     ActionMetadata = 'action.yaml.tmpl',
     WorkflowMetadata = 'workflow.yaml.tmpl',
-    RuleConfig = 'rule.yaml.tmpl'
+    RuleConfig = 'rule.yaml.tmpl',
+    SensorMetadata = 'sensor.yaml.tmpl'
 }
